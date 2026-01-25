a74bd0d87af25cc9dfa07d17f78f58395dcfccc1
Sami Farin <hvtaifwkbgefbaei@gmail.com>
2026-01-25 15:53:08 +0200

    .

--- src/core/net-nonblock.c
+++ src/core/net-nonblock.c
@@ -96,72 +96,6 @@ GCancellable *net_gethostbyname_nonblock(const char *addr, GResolverNameLookupFl
 
 	g_return_val_if_fail(addr != NULL, FALSE);
 
-<<<<<<< HEAD
-	pid = fork();
-	if (pid > 0) {
-		/* parent */
-		pidwait_add(pid);
-		return pid;
-	}
-
-	if (pid != 0) {
-		/* failed! */
-		g_warning("net_connect_thread(): fork() failed! "
-			  "Using blocking resolving");
-	}
-
-	memset(&rec, 0, sizeof(rec));
-	rec.error = net_gethostbyname(addr, &rec.ip4, &rec.ip6);
-	if (rec.error == 0) {
-		errorstr = NULL;
-	} else {
-		errorstr = net_gethosterror(rec.error);
-		rec.errlen = errorstr == NULL ? 0 : strlen(errorstr)+1;
-	}
-
-	i_io_channel_write_block(pipe, &rec, sizeof(rec));
-	if (rec.errlen != 0)
-		i_io_channel_write_block(pipe, (void *) errorstr, rec.errlen);
-
-	if (pid == 0)
-		_exit(99);
-
-	/* we used blocking lookup */
-	return 0;
-}
-
-/* get the resolved IP address */
-int net_gethostbyname_return(GIOChannel *pipe, RESOLVED_IP_REC *rec)
-{
-	rec->error = -1;
-	rec->errorstr = NULL;
-
-	fcntl(g_io_channel_unix_get_fd(pipe), F_SETFL, O_NONBLOCK);
-
-	/* get ip+error */
-	if (i_io_channel_read_block(pipe, rec, sizeof(*rec)) == -1) {
-		rec->errorstr = g_strdup_printf("Host name lookup: %s",
-						g_strerror(errno));
-		return -1;
-	}
-
-	if (rec->error) {
-		/* read error string, if we can't read everything for some
-		   reason, just ignore it. */
-		rec->errorstr = g_malloc0(rec->errlen+1);
-		i_io_channel_read_block(pipe, rec->errorstr, rec->errlen);
-	}
-
-	return 0;
-}
-
-/* Kill the resolver child */
-void net_disconnect_nonblock(int pid)
-{
-	g_return_if_fail(pid > 0);
-
-	kill(pid, SIGKILL);
-=======
 	resolver = g_resolver_get_default();
 	cancellable = g_cancellable_new();
 	data = g_new0(NET_GETHOSTBYNAME_CALLBACK_DATA, 1);
@@ -178,5 +112,4 @@ void net_disconnect_nonblock(int pid)
 	                                (GAsyncReadyCallback) net_gethostbyname_callback, data);
 #endif
 	return cancellable;
->>>>>>> upstream/master
 }
